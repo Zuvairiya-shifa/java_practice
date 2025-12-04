8ef7c724982a22f2639bf363f6ff4674266a4843
ZUVAIRIYA SHIFA S <sulthanshifa0@gmail.com>
2025-12-04 16:39:24 +0530

    oddeven

---
+++ string_method.java
@@ -0,0 +1,19 @@
+public class string_method{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        String input=sc.nextLine();
+        if (input.IsAlphabetic()){
+            System.out.println("alphabet");
+
+        }
+        else if (input.IsNumeric()){
+            System.out.println("number");
+
+        }
+        string spl="!@#$%^&*()_+";
+        else if (input==spl){
+            System.out.println("special character");
+        }
+
+    }
+}
