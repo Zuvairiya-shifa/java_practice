4d9546392023bf71fc0a6584f9f768af1be4d3e8
ZUVAIRIYA SHIFA S <sulthanshifa0@gmail.com>
2025-12-04 13:55:06 +0530

    swapping

---
+++ swapping.java
@@ -0,0 +1,11 @@
+public class swapping{
+    public static void main(String[] args) {
+        int a =10;
+        int b=20;
+        System.out.println("Before Swapping:"+"a="+a+"b="+b);
+        int temp;
+        temp=a;
+        a=b;b=temp;
+        System.out.println("After Swapping:"+"a="+a+"b="+b);
+
+    }
